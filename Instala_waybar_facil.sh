7a8d360caced907d763a09b729a037ea9d10be21
MaxiRamos-byte <maximilianoramossanchez42@gmail.com>
2025-12-22 18:16:17 -0600

    Uso de menu para su instalacion
    Ahora permite compilar yay si es que no lo tienes (incluye la fuente faltante)

--- Instala_waybar_facil.sh
+++ Instala_waybar_facil.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "Tienes yay?"
+echo "1) SI"
+echo "2) NO"
+
+read -p "Introduce el número: " opcion
+
+case $opcion in
+1)
 echo "Comenzando con la instalacion de waybar (Ya incluye un tema)"
 
 sudo pacman --noconfirm -S waybar
@@ -13,12 +21,59 @@ sudo pacman --noconfirm -S wlogout
 
 sudo pacman --noconfirm -S mission-center
 
+echo "Instalamos las fuentes necesarias"
+
+yay --noconfirm -S ttf-jetbrains-mono-nerd
+
+echo "Cerramos waybar"
+
+pkill waybar
+
 echo "Procedemos a copiar la configuracion a la carpeta necesaria"
 cp -r waybar ~/.config
 
-echo "Reiniciamos waybar"
+echo "volvemos a iniciar waybar"
+waybar &
+
+echo "Se ha creado un archivo de texto en tus documentos donde podras conocer las funciones de los dots"
+cp -r README/Funciones.txt ~/Documentos
+
+echo "Lo encontraras en tu carpeta de Documentos en la carpeta README"
+
+2)
+echo "Instalamos yay"
+
+sudo pacman --noconfirm -S --needed base-devel git
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+
+echo "Comenzando con la instalacion de waybar (Ya incluye un tema)"
+
+sudo pacman --noconfirm -S waybar
+
+echo "Ya instalado se ejecuta para una configuracion inicial"
+waybar &
+
+echo "Instalaremos las dependencias necesarias para su correcto funcionamiento"
+
+sudo pacman --noconfirm -S wlogout
+
+sudo pacman --noconfirm -S mission-center
+
+echo "Instalamos las fuentes necesarias"
+
+yay --noconfirm -S ttf-jetbrains-mono-nerd
+
+echo "Cerramos waybar"
 
 pkill waybar
+
+echo "Procedemos a copiar la configuracion a la carpeta necesaria"
+cp -r waybar ~/.config
+
+echo "volvemos a iniciar waybar"
 waybar &
 
 echo "Se ha creado un archivo de texto en tus documentos donde podras conocer las funciones de los dots"
